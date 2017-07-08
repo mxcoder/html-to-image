5decd045d902cf95f6254704a0b39e7ed0fa84ca
Ricardo Vega <ricardo@gumgum.com>
2017-07-07 22:32:08 -0500

    Working draft

--- src/lib/screenshot.js
+++ src/lib/screenshot.js
@@ -19,59 +19,38 @@ function screenshot(html, width, height, timeout) {
     }, (client) => {
         const { Runtime, Network, Emulation, Page } = client;
 
+        Page.loadEventFired(captureScreenshot);
+        Network.setCacheDisabled({ cacheDisabled: true });
+        Network.requestWillBeSent(({ requestId, request }) => {
+            console.log('requestWillBeSent', requestId, request.url);
+        });
+        Runtime.consoleAPICalled(({type, args}) => {
+            console.log('consoleAPICalled', mtype);
+        });
+        Emulation.setDeviceMetricsOverride({
+            width: width,
+            height: height,
+            deviceScaleFactor: 0,
+            mobile: false,
+            fitWindow: false,
+        });
+        Emulation.setVisibleSize({
+            width: width,
+            height: height,
+        });
+
         function init() {
             return Promise.all([
                 Page.enable(),
                 Runtime.enable(),
                 Network.enable(),
-            ]).then(() => {
-                return Page.loadEventFired(captureScreenshot);
-            }).then(() => {
-                console.log('setCacheDisabled');
-                return Network.setCacheDisabled({ cacheDisabled: true });
-            }).then(() => {
-                console.log('requestWillBeSent');
-                return Network.requestWillBeSent(({ requestId, request }) => {
-                    console.log('requestWillBeSent', requestId, request.url);
-                });
-            }).then(() => {
-                return Runtime.consoleAPICalled(({type, args}) => {
-                    console.log('consoleAPICalled', mtype);
-                });
-            });
+            ]);
         }
 
-        function setDeviceMetricsOverride() {
-            console.log('screenshot', 'setDeviceMetricsOverride');
-            return Emulation.setDeviceMetricsOverride({
-                width: width,
-                height: height,
-                deviceScaleFactor: 0,
-                mobile: false,
-                fitWindow: false,
-            });
-        }
-
-        function setVisibleSize() {
-            console.log('screenshot', 'setVisibleSize');
-            return Emulation.setVisibleSize({
-                width: width,
-                height: height,
-            })
-        };
-
-        function loadContent() {
-            console.log('screenshot', 'loadContent');
-            return Page.navigate({ url: 'about:blank' })
-                .then(setDocumentContent);
-        }
-
-        function setDocumentContent({ frameId }) {
-            console.log('screenshot', 'setDocumentContent', frameId, baseHTML + `<div style="position:absolute;width:${width}px;height:${height}px;background:rgba(0,0,0,0.25)">` + html + '</div>');
-            return Page.setDocumentContent({
-                frameId: frameId.toString(),
-                html: baseHTML + `<div style="position:absolute;width:${width}px;height:${height}px;background:rgba(0,0,0,0.25)">` + html + '</div>',
-            });
+        function navigate() {
+            console.log('screenshot', 'navigate');
+            const markup = baseHTML + `<div style="position:absolute;width:${width}px;height:${height}px;background:rgba(0,0,0,0.25)">` + html + '</div>';
+            return Page.navigate({ url: 'data:text/html;base64,' + new Buffer(markup).toString('base64') })
         }
 
         function captureScreenshot() {
@@ -84,19 +63,18 @@ function screenshot(html, width, height, timeout) {
             client.close(() => {
                 console.log('screenshot end');
                 deferred.resolve(data);
-                // const buffer = new Buffer(data, 'base64');
-                // fs.writeFile('screenshot.png', buffer, 'base64');
             });
         }
 
         init()
-        .then(setDeviceMetricsOverride)
-        .then(setVisibleSize)
-        .then(loadContent)
+        .then(navigate)
         .catch((err) => {
             console.log(err);
             deferred.reject(err);
         });
+    }).on('error', (err) => {
+        console.error(err);
+        deferred.reject(err);
     });
     return deferred.promise;
 }
